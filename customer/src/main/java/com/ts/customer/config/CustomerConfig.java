36f1ed16d66afd5da266a3fd9510b9c63bdbfbec
Cui Jikang <cui_ji@worksap.co.jp>
2018-05-28 18:03:44 +0800

    init commit

---
+++ customer/src/main/java/com/ts/customer/config/CustomerConfig.java
@@ -0,0 +1,10 @@
+package com.ts.customer.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ts.customer.*")
+public class CustomerConfig {
+
+}
